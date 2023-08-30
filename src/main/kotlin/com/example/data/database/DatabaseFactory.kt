b262c205e98551108de62bccc80ca1d7b19a094d
RinaJoseArulselvan <135804163+RinaJoseArulselvan@users.noreply.github.com>
2023-08-30 15:45:48 +0530

    Update DatabaseFactory.kt

--- src/main/kotlin/com/example/data/database/DatabaseFactory.kt
+++ src/main/kotlin/com/example/data/database/DatabaseFactory.kt
@@ -11,20 +11,29 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 
 object DatabaseFactory  {
-    private fun hikari(): HikariDataSource {
-        val config = HikariConfig().apply{
-            jdbcUrl         = "jdbc:postgresql://localhost:5432/shoppingcart"
-            driverClassName = "org.postgresql.Driver"
-            username        = "postgres"
-            password        = "rina2002"
-            maximumPoolSize = 3
-        }
+    private fun hikari(isTest: Boolean): HikariDataSource {
+        val config = if (isTest) {
+            HikariConfig().apply {
+                jdbcUrl = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
+                driverClassName = "org.h2.Driver"
+                username = "sa"
+                password = ""
+            }
+            }else{
+            HikariConfig().apply {
+                jdbcUrl = "jdbc:postgresql://localhost:5432/shoppingcart"
+                driverClassName = "org.postgresql.Driver"
+                username = "postgres"
+                password = "rina2002"
+                maximumPoolSize = 3
+            }
+            }
         config.validate()
         return HikariDataSource(config)
     }
 
-    fun init() {
-        val database = Database.connect(hikari())
+    fun init(isTest: Boolean = false) {
+        val database = Database.connect(hikari(isTest))
         transaction(database) {
             SchemaUtils.create(ItemsEntity)
         }
